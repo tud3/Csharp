795b9fc5241533f21af28099196cf0c3d0ebf8dc
tud3 <tunuppi@hotmail.com>
2014-03-12 11:38:18 +0200

    Aloitus

---
+++ Projektit/PeliVaraus/PeliVaraus/Connection.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PeliVaraus
+{
+    class Connection
+    {
+        private string connString = "127.0.0.1;port=3306;username=root;";
+        //private string connString = "server=mysql.labranet.jamk.fi;port=3306;username=G7791;password=zZ9EDWkh59HAOQe6odjx7znq7vIQDa1i";
+        private MySqlConnection myConn;
+
+       
+
+        public void openConnection()
+        {
+                myConn = new MySqlConnection(connString);
+                myConn.Open();
+                MessageBox.Show("Nyt se on auki...");
+                
+        }
+
+        public void closeConnection() 
+        {
+            myConn.Close();
+            MessageBox.Show("...ja nyt se on kii!");
+        }
+
+    }
+}
---
+++ Projektit/PeliVaraus/PeliVaraus/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PeliVaraus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Connection connection = new Connection();
+        private void btnTestaus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection.openConnection();                
+            }
+    
+            catch
+            {
+                MessageBox.Show("Virhe!");
+            }
+
+            finally
+            {
+                connection.closeConnection();
+            }
+        }
+
+        private void btnHenk_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                string sql = "use g7791; SELECT * FROM henkilo;";
+                connection.openConnection();
+                MySqlDataAdapter myDA = new MySqlDataAdapter();
+                MySqlCommandBuilder cmd = new MySqlCommandBuilder(myDA);
+                DataSet ds = new DataSet();
+                
+                
+
+            }
+            catch 
+            {
+                MessageBox.Show("Virhe!!");
+            }
+            finally 
+            {
+                connection.closeConnection();
+            }
+        }
+    }
+}
+
